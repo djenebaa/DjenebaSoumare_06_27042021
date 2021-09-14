c847397bd501ce32e280aead67cf462e2496789f
djenebaa <djenebasoum98@icloud.com>
2021-09-14 17:18:30 +0200

    ajout de la sécurite

---
+++ Back-end/README.md
@@ -0,0 +1,3 @@
+Pour lancer le backend du projet 6 il vous faut:
+Install express 
+Puis lancer nodemon server
--- Back-end/controllers/user.js
+++ Back-end/controllers/user.js
@@ -1,8 +1,7 @@
 const bcrypt = require("bcrypt");
 const jwt = require('jsonwebtoken');
-
 const User = require("../models/user");
-
+require('dotenv').config();
 exports.signup = (req, res, next) => {
   bcrypt
     .hash(req.body.password, 10)
@@ -14,7 +13,7 @@ exports.signup = (req, res, next) => {
       user
         .save()
         .then(() => res.status(201).json({ message: "User created" }))
-        .catch((error) => res.status(400).json({ error }));
+        .catch((error) => res.status(400).json({ error}));
     })
     .catch((error) => res.status(500).json({ error }));
 };
